f01a336d6e79c56465402c2a55e0ceea4d932b31
Paul <lakehuronshores@gmail.com>
2022-11-14 01:12:57 -0500

    update db creation
    Simplified database creation script
    Update compose file to create\default api databse

--- create_database.sql
+++ create_database.sql
@@ -1,101 +1,29 @@
+CREATE USER dbuser with password 'hello';
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO dbuser;
 
+CREATE TABLE users (
+    _id integer PRIMARY KEY,
+    username varchar(20) UNIQUE NOT NULL,
+    display_name varchar(20) UNIQUE NOT NULL,
+    password varchar(120) NOT NULL,
+    email varchar(120) UNIQUE NOT NULL,
+    email_confirmed boolean DEFAULT false
+);
 
-
-CREATE USER IF NOT EXISTS 'dbuser'@'api';
-
-CREATE SEQUENCE public.blog__id
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE	
-    CACHE 1;
-
-ALTER TABLE public.blog__id OWNER TO dbadmin;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
-CREATE TABLE public.blog (
-    _id integer DEFAULT nextval('public.blog__id'::regclass) NOT NULL,
+CREATE TABLE blog (
+    _id integer PRIMARY KEY, 
     author integer NOT NULL,
-    body character varying NOT NULL,
+    body varchar NOT NULL,
     created bigint NOT NULL,
     edited bigint NOT NULL,
-    title character varying(120) NOT NULL
+    title varchar(120) NOT NULL,
+    FOREIGN KEY(author) REFERENCES users(_id)
 );
 
-ALTER TABLE public.blog OWNER TO dbadmin;
-
-CREATE SEQUENCE public.user_tokens__id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER TABLE public.user_tokens__id_seq OWNER TO dbadmin;
-
-CREATE TABLE public.user_tokens (
-    _id integer DEFAULT nextval('public.user_tokens__id_seq'::regclass) NOT NULL,
+CREATE TABLE user_tokens (
+    _id integer PRIMARY KEY, 
     token uuid NOT NULL,
     user_id integer NOT NULL,
-    expires bigint NOT NULL
+    expires bigint NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(_id)
 );
-
-ALTER TABLE public.user_tokens OWNER TO dbadmin;
-
-CREATE SEQUENCE public.users__id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER TABLE public.users__id_seq OWNER TO dbadmin;
-
-CREATE TABLE public.users (
-    username character varying(20) NOT NULL,
-    display_name character varying(20) NOT NULL,
-    password character varying(120) NOT NULL,
-    email character varying(120) NOT NULL,
-    email_confirmed boolean DEFAULT false NOT NULL,
-    _id integer DEFAULT nextval('public.users__id_seq'::regclass) NOT NULL
-);
-
-ALTER TABLE public.users OWNER TO dbadmin;
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT display_name_unique UNIQUE (display_name);
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT email_unique UNIQUE (email);
-
-ALTER TABLE ONLY public.blog
-    ADD CONSTRAINT pkey_blog PRIMARY KEY (_id);
-
-ALTER TABLE ONLY public.user_tokens
-    ADD CONSTRAINT user_tokens_pkey PRIMARY KEY (_id);
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT username_unique UNIQUE (username);
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (_id);
-
-ALTER TABLE ONLY public.blog
-    ADD CONSTRAINT fkey_author FOREIGN KEY (author) REFERENCES public.users(_id);
-
-ALTER TABLE ONLY public.user_tokens
-    ADD CONSTRAINT user_tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(_id) ON DELETE CASCADE;
-
-GRANT ALL ON SEQUENCE public.blog__id TO dbadmin;
-GRANT ALL ON SEQUENCE public.blog__id TO "dbuser";
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.blog TO dbuser;
-GRANT ALL ON SEQUENCE public.user_tokens__id_seq TO "dbuser";
-GRANT ALL ON SEQUENCE public.user_tokens__id_seq TO dbadmin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.user_tokens TO "dbuser";
-GRANT ALL ON SEQUENCE public.users__id_seq TO "dbuser";
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.users TO "dbuser";
